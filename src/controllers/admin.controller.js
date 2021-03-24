8d7da6e0207892b750f942d994fe9a92c75f7f6f
MarianoAmado <mariano.amado@dixtra.co>
2021-03-24 17:19:19 -0300

    * Added helper to validate format of name fields * Added addTopic operation in admin controller (create_topic command) (WIP)

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,31 @@
+const commonViews = require('../views/common.views');
+const blocksViews = require('../views/blocks.views');
 
+const { validName } = require('../helpers/common.helper');
 /*
   Controller para acciones de los admins (crear topics, aprobar posts, etc)
 */
+
+function addTopic(req, res) {
+  const name = req.text;
+
+  if (validName(name)) {
+    // algo asi
+    // Topic.findOne({ name }).then((topic) => {
+      // if (topic) res.error('ERR');
+      // else {
+        // y asi
+        // Topic.create({ name }).then((topic) => {
+          const text = `Topic '${name}' created!`;
+          const message = blocksViews.plainText(text);
+
+          res.renderBlocks([message], true);
+        // });
+      // }
+    // });
+  } else {
+    res.renderSlack(commonViews.commandError(`Invalid topic name '${name}'`));
+  }
+}
+
+module.exports = { addTopic };
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+module.exports = {
+  nameRegex: /[A-Z][a-z]/,
+};
