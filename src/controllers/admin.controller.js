3263efe0145aa2483170f7998e148eb21cdb781e
MarianoAmado <mariano.amado@dixtra.co>
2021-03-31 14:46:08 -0300

    addTopic: using models & creating topic on team

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,28 +1,26 @@
 const commonViews = require('../views/common.views');
 const blocksViews = require('../views/blocks.views');
+const { Topic } = require('../models');
 
 const { validName } = require('../helpers/common.helper');
+
 /*
   Controller para acciones de los admins (crear topics, aprobar posts, etc)
 */
 
-function addTopic(req, res) {
-  const name = req.text;
+async function addTopic(req, res) {
+  const { text: name, team } = req;
 
   if (validName(name)) {
-    // algo asi
-    // Topic.findOne({ name }).then((topic) => {
-      // if (topic) res.error('ERR');
-      // else {
-        // y asi
-        // Topic.create({ name }).then((topic) => {
-          const text = `Topic '${name}' created!`;
-          const message = blocksViews.plainText(text);
+    if (await Topic.findOne({ name })) res.error('ERR');
+    else {
+      await team.createTopic({ name });
 
-          res.renderBlocks([message], true);
-        // });
-      // }
-    // });
+      const text = `Topic '${name}' created!`;
+      const message = blocksViews.plainText(text);
+
+      res.renderBlocks([message], true);
+    }
   } else {
     res.renderSlack(commonViews.commandError(`Invalid topic name '${name}'`));
   }
