ade75d45db34afe8c5bc634f545158146135c18d
Diego del Valle <diego.valle@dixtra.co>
2021-03-31 14:30:17 -0300

    rollback change

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const middleware = require('../middlewares/user.middleware');
 const controller = require('../controllers/user.controller');
 
+/* GET users listing. */
+router.get('/users', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
 router.post('/users', middleware.getUser, controller.getPosts);
 
 module.exports = router;
