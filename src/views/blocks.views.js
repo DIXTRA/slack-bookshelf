65359e85a3b98c793e1e4d472af78219e5d7c85e
Francisco Crizul <francisco.crizul@dixtra.com>
2021-06-02 17:07:18 -0300

    add new blocks

--- src/views/blocks.views.js
+++ src/views/blocks.views.js
@@ -16,6 +16,30 @@ function divider() {
   return { type: 'divider' };
 }
 
+function image(image_url, alt_text = 'image') {
+  return { type: 'image', image_url , alt_text};
+}
+
+function section(text) {
+  return { type: 'section', text};
+}
+
+function sectionWithImage(text, image) {
+  return { type: 'section', text , accessory: image};
+}
+
+function actions(elements = []) {
+  return { type: 'actions', elements};
+}
+
+function action(type = "button", text, style = 'primary', value) {
+  return { type, text, style, value};
+}
+
+function text(type = 'mrkdwn', text, emoji) {
+  return { type, text, emoji};
+}
+
 /*
   Plain text UI block
 */
@@ -71,4 +95,11 @@ module.exports = {
   base,
   markdown,
   divider,
+  image,
+  section,
+  sectionWithImage,
+  context,
+  actions,
+  action,
+  text
 };
