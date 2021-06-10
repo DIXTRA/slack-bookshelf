35903a1111834c598004fd1a3b9ff14db8687e01
Diego del Valle <diego.valle@dixtra.co>
2021-06-10 16:41:01 -0300

    fix trailing whitespaces

---
+++ src/middlewares/interactivity.middleware.js
@@ -0,0 +1,46 @@
+const { Team, User } = require('../models');
+const { getUser } = require('../helpers/slack.helper');
+
+async function getInteractiveEventUserAndTeam(req, res, next) {
+  const { user: { id: userId, team_id: teamId } } = JSON.parse(req.body.payload);
+
+  const team = await Team.findOne({
+    slackId: teamId,
+  });
+
+  if (team) {
+    req.team = team;
+
+    const user = await User.findOne({
+      where: {
+        slackId: userId,
+        TeamId: team.id,
+      }
+    });
+
+    if (user) {
+      req.user = user;
+
+      next();
+    } else { // add user to the team
+      const user = await getUser(req.team.token, user_id);
+
+      req.user = await team.createUser({
+        slackId: user.id,
+        displayName: user.profile.display_name_normalized,
+        profilePicture: user.profile.image_24,
+        username: user.name,
+        isAdmin: user.is_admin || user.is_owner,
+      });
+
+      next();
+    }
+  } else {
+    // TODO: replace with error block
+    res.error('team not found');
+  }
+}
+
+module.exports = {
+    getInteractiveEventUserAndTeam,
+}
