c0c2244832fb6871fd101d8a402587d6a0c8e9bd
Diego del Valle <diego.valle@dixtra.co>
2021-07-16 15:27:19 -0300

    remove ese token muchacho

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -61,11 +61,6 @@ async function addTopic(req, res) {
   }
 }
 
-// { "type": "block_actions", "user": { "id": "U01QWBBE4SZ", "username": "diego.valle", "name": "diego.valle", "team_id": "T01QSKEL1QW" }, "api_app_id": "A01RYPT4Q72", "token": "yQxF4X45RAmngqbXRKB34Fww", "container": { "type": "message", "message_ts": "1619634961.000200", "channel_id": "D01RC2X5L49", "is_ephemeral": true }, "trigger_id": "2011973345219.1842660681846.b8d4c1067ca94fc07c4c4741cfe39b36", "team": { "id": "T01QSKEL1QW", "domain": "slack-bookshelf" }, "enterprise": null, "is_enterprise_install": false, "channel": { "id": "D01RC2X5L49", "name": "directmessage" }, "state": { "values": { } }, "response_url": "https:\/\/hooks.slack.com\/actions\/T01QSKEL1QW\/2011762010354\/bx8Cc56AX7tX9oNpsT3yB9w7", "actions": [{ "action_id": "W7Mlk", "block_id": "Wnv\/s", "text": { "type": "plain_text", "text": "Approve", "emoji": true }, "value": "approve", "style": "primary", "type": "button", "action_ts": "1619634967.532538" }] }
-async function approveArticle(req, res) {
-  const { team, user } = req;
-}
-
 async function listTopicLinks(req, res) {
   const { text, team } = req;
 
@@ -103,4 +98,4 @@ async function listTopicLinks(req, res) {
   }
 }
 
-module.exports = { addTopic, listTopicLinks, approveArticle };
+module.exports = { addTopic, listTopicLinks };
